fcb36e3df16e94c17999d13577e0f424cf14b662
Argi Avetisyan <argishtiavetis@gmail.com>
2017-09-03 22:35:54 -0700

    Update classes101.cs

--- misc/classes101.cs
+++ misc/classes101.cs
@@ -6,25 +6,34 @@ using System.Threading.Tasks;
 
 namespace dummy
 {
-
     class Program
     {
         static void Main(string[] args)
         {
             var cars = new List<object>();
-            cars.Add(new Car());
-            cars.Add(new Car(2014, "BMW", "Germany"));
-            cars.Add(new Car(2017, "TESLA", "USA"));
+
+            var car1 = new Car();
+            car1.Born = 2015;
+            cars.Add(car1);
+
+            var car2 = new Car();
+            car2.Born = 2014;
+            cars.Add(car2);
 
             Console.WriteLine(cars.Count);
 
             foreach(var car in cars)
             {
-                Console.WriteLine(car);
+                Console.WriteLine(((Car)car).Born);
             }
 
             Console.ReadLine();
         }
+    }
+
+    class Car
+    {
+        public int Born { get; set; }
     }
 
 }
